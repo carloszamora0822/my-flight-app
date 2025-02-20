fd69d31f7b50967a4b5ae493203d4ff6ae071f93
Carlos <carloszamorawork@Carloss-MacBook-Pro.local>
2025-02-20 17:27:19 -0600

    Vesta restart

--- client/src/components/FlightItem.js
+++ client/src/components/FlightItem.js
@@ -1,13 +1,15 @@
+// client/src/components/FlightItem.js
 import React from 'react';
 
 function FlightItem({ flight, onDelete }) {
   return (
-    <div className="flight-item">
-      <p>
-        <strong>Time:</strong> {flight.time} | <strong>Callsign:</strong> {flight.callsign} | <strong>Type:</strong> {flight.type} | <strong>Destination:</strong> {flight.destination}
-      </p>
+    <li style={{ display: 'flex', justifyContent: 'space-between', padding: '4px 0' }}>
+      <span style={{ width: '20%' }}>{flight.time}</span>
+      <span style={{ width: '30%' }}>{flight.callsign}</span>
+      <span style={{ width: '25%' }}>{flight.type}</span>
+      <span style={{ width: '25%' }}>{flight.destination}</span>
       <button onClick={onDelete}>Delete</button>
-    </div>
+    </li>
   );
 }
 
