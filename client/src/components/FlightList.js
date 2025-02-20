5af39a7e9178bc6cb24cb956fffc9fbff4dcceab
Carlos <carloszamorawork@Carloss-MacBook-Pro.local>
2025-02-20 17:36:34 -0600

    Making more appropriate variable names

--- client/src/components/FlightList.js
+++ client/src/components/FlightList.js
@@ -1,4 +1,3 @@
-// client/src/components/FlightList.js
 import React from 'react';
 import FlightItem from './FlightItem';
 
@@ -6,17 +5,9 @@ function FlightList({ flights, deleteFlight }) {
   return (
     <div className="recent-submissions">
       <h2>Recent Flights</h2>
-      {/* Header Row */}
-      <div className="flight-header" style={{ fontWeight: 'bold', padding: '8px 0' }}>
-        <span style={{ display: 'inline-block', width: '20%' }}>Time</span>
-        <span style={{ display: 'inline-block', width: '30%' }}>Name</span>
-        <span style={{ display: 'inline-block', width: '25%' }}>Flight Type</span>
-        <span style={{ display: 'inline-block', width: '25%' }}>Flight Number</span>
-      </div>
-      {/* Flight Items */}
       <div className="history-list">
         {flights.length > 0 ? (
-          <ul style={{ listStyleType: 'none', padding: 0 }}>
+          <ul>
             {flights.map((flight, index) => (
               <FlightItem
                 key={index}
